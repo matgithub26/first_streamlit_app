ec9ed2ebb5f379c55f3eb7d5bc3006574905bcec
matgithub26 <133324764+matgithub26@users.noreply.github.com>
2023-05-13 15:55:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,6 +48,8 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
 
+add_my_fruit_response = requests.get("https://fruityvice.com/api/fruit/"+ add_my_fruit )
+
 
 
 
